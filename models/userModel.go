514d1ca450dba115bf6e14c1723e218863f65126
tezarsurya <tzr.surya@gmail.com>
2022-12-13 20:04:21 +0700

    add some fields and password hash in User

--- models/userModel.go
+++ models/userModel.go
@@ -1,14 +1,19 @@
 package models
 
 import (
-	"database/sql"
 	"hello-from-go/config"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
-	ID       uint   `json:"id,omitempty"`
-	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password,omitempty" binding:"required,min=8"`
+	ID        uint        `json:"id,omitempty"`
+	Email     string      `json:"email" binding:"required,email"`
+	Password  string      `json:"password,omitempty" binding:"required,min=8"`
+	CreatedAt string      `json:"createdAt"`
+	UpdatedAt string      `json:"updatedAt,omitempty"`
+	DeletedAt interface{} `json:"deletedAt,omitempty"`
 }
 
 var db = config.ConnectDB()
@@ -22,26 +27,31 @@ func (u *User) GetAll() []User {
 	}
 
 	for rows.Next() {
-		errScan := rows.Scan(&u.ID, &u.Email, &u.Password)
+		errScan := rows.Scan(&u.ID, &u.Email, &u.Password, &u.CreatedAt, &u.UpdatedAt, &u.DeletedAt)
 		if errScan != nil {
 			panic(errScan)
 		}
 		*u = User{
-			Email:    u.Email,
-			Password: u.Password,
+			ID:        u.ID,
+			Email:     u.Email,
+			CreatedAt: u.CreatedAt,
+			UpdatedAt: u.UpdatedAt,
+			DeletedAt: u.DeletedAt,
 		}
 		users = append(users, *u)
 	}
-
 	return users
 }
 
-func (u *User) Create() sql.Result {
-	result, err := db.Exec("INSERT INTO users (email, password) VALUES (?, ?)", u.Email, u.Password)
-	if err != nil {
-		panic(err)
+func (u *User) Create() bool {
+	hashP, errHash := bcrypt.GenerateFromPassword([]byte(u.Password), 10)
+	if errHash != nil {
+		panic(errHash)
 	}
-	return result
+
+	created := time.Now()
+	_, errExec := db.Exec("INSERT INTO users (email, password, created_at, updated_at) VALUES (?, ?, ?, ?)", u.Email, string(hashP), created, created)
+	return errExec == nil
 }
 
 func CheckEmail(user User) bool {
